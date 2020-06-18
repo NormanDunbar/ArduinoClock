a994508c9147adb382be29006c482157bd70eb43
Norman Dunbar <norman@dunbar-it.co.uk>
2020-06-18 12:05:45 +0100

    ATtiny85 version added - NOT YET COMPLETE or able to compile.

---
+++ Sources/RTC_ATtiny85/RTC_ATtiny85.ino
@@ -0,0 +1,94 @@
+#include <TinyWireM.h>              // for I2C communication
+#include <USI_TWI_Master.h>
+
+#include <LiquidCrystal_I2C.h>      // for LCD
+#include <TinyRTClib.h>             // For RTC.
+
+LiquidCrystal_I2C lcd(0x3f, 16, 2); // create LCD with I2C address 0x3F, 16 characters per line, 2 lines
+RTC_DS3231 rtc;                     // create rtc for the DS3231 RTC module, address is fixed at 0x68
+
+
+/* WIRING DIAGRAM:
+ *  
+ *  ATTINY85    PIN     RTC     LCD DISPLAY
+ *  ---------------------------------------
+ *     PB0       5      SDA         SDA
+ *     PB2       7      SCL         SCL
+ *      5V       8      VCC         VCC
+ *     GND       4      GND         GND 
+ *     
+ *
+ *  The updateLCD() function below is a greatly modified version of the code at:
+ *  https://www.circuitbasics.com/how-to-use-a-real-time-clock-module-with-the-arduino
+ *  and was modified by Norman Dunbar to use built in RTCLib functions to
+ *  format the date and time for display on the LCD.
+ *  
+ *  This entire file is a conversion of the original code, above, for the ATtiny85
+ *  microcontroller. Just to see if it could be done!
+ */
+   
+ 
+/*
+   function to update LCD text
+*/
+void updateLCD()
+{
+    // get time and date from RTC and save in variables
+    DateTime rtcTime = rtc.now();
+
+    /*
+     * Buffers to format the date and time (on separate lines of the LCD)
+     * 
+     * Parameters are:
+     *
+        | specifier | output                                                 |
+        |-----------|--------------------------------------------------------|
+        | YYYY      | the year as a 4-digit number (2000--2099)              |
+        | YY        | the year as a 2-digit number (00--99)                  |
+        | MM        | the month as a 2-digit number (01--12)                 |
+        | MMM       | the abbreviated English month name ("Jan"--"Dec")      |
+        | DD        | the day as a 2-digit number (01--31)                   |
+        | DDD       | the abbreviated English day of the week ("Mon"--"Sun") |
+        | AP        | either "AM" or "PM"                                    |
+        | ap        | either "am" or "pm"                                    |
+        | hh        | the hour as a 2-digit number (00--23 or 01--12)        |
+        | mm        | the minute as a 2-digit number (00--59)                |
+        | ss        | the second as a 2-digit number (00--59)                |
+    
+        If either "AP" or "ap" is used, the "hh" specifier uses 12-hour mode
+        (range: 01--12). Otherwise it works in 24-hour mode (range: 00--23).
+    
+        The specifiers within _buffer_ will be overwritten with the appropriate
+        values from the DateTime. Any characters not belonging to one of the
+        above specifiers are left as-is.
+     */
+     
+    char dateBuffer[] = "DD-MMM-YYYY DDD";
+    char timeBuffer[] = "hh:mm:ss AP";
+    
+    // move LCD cursor to upper-left position
+    lcd.setCursor(0, 0);
+    lcd.print(rtcTime.toString(dateBuffer));
+
+    // move LCD cursor to lower-left position
+    lcd.setCursor(0, 1);
+    lcd.print(rtcTime.toString(timeBuffer));
+}
+
+
+void setup()
+{
+    lcd.begin();        // initialize lcd
+    lcd.backlight();    // switch-on lcd backlight
+  
+    rtc.begin();        // initialize rtc
+}
+
+
+void loop()
+{
+    updateLCD();    // update LCD text
+    delay(1000);    // No point being too quick!
+}
+
+
